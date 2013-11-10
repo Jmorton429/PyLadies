540cc5919db25e53e9e538d1a818faa4016737be
jmkoni <jmkoni@icloud.com>
2013-11-10 16:09:22 -0500

    bbmatson - adding end game function and game chooser

--- Beginner/Round 2/2013-11-10 #4/casinogame.py
+++ Beginner/Round 2/2013-11-10 #4/casinogame.py
@@ -2,48 +2,65 @@ import random
 from random import choice
 from random import randint
 
-
+def booted():
+    print "You have to leave."
+    exit(0)
 
 def enter_casino():
-	print "You just entered a casino with slots, blackjack, and a guessing game."
-	game = raw_input("What would you like to play? ")
-	if game == "slots":
-		slot_machine()
-	elif game == "blackjack":
-		blackjack()
-	elif game == "guessing game":
-		start_guessing_game()
-	else:
-		FantasyCasino()
-		
+    print "You just entered a casino with slots, blackjack, and a guessing game."
+    game = raw_input("What would you like to play? ")
+    if game == "slots":
+        slot_machine()
+    elif game == "blackjack":
+        blackjack()
+    elif game == "guessing game":
+        start_guessing_game()
+    else:
+        FantasyCasino()
+
+def pick_game():
+    if money == 0:
+        print "Get out"
+        booted()
+    else:
+        game = raw_input("What would you like to play? ")
+        if game == "slots":
+            slot_machine()
+        elif game == "blackjack":
+            blackjack()
+        elif game == "guessing game":
+            start_guessing_game()
+        else:
+            FantasyCasino()
 
 def slot_machine():
-	fruits = ['apple', 'watermelon', 'strawberry', 'banana']
-	
-	index  = random.randint(0, 3)
-	fruit1 = fruits[index]
+    fruits = ['apple', 'watermelon', 'strawberry', 'banana']
+    money = 0
+    index  = random.randint(0, 3)
+    fruit1 = fruits[index]
 
-	index  = random.randint(0, 3)
-	fruit2 = fruits[index]
+    index  = random.randint(0, 3)
+    fruit2 = fruits[index]
 
-	index  = random.randint(0, 3)
-	fruit3 = fruits[index]
+    index  = random.randint(0, 3)
+    fruit3 = fruits[index]
 
-	print fruit1 + ' / ' + fruit2 + ' / ' + fruit3
-	if fruit1 == fruit2 == fruit3:
-		print 'You won!!!'
-	else:
-		print 'Maybe next time'
-		
+    print fruit1 + ' / ' + fruit2 + ' / ' + fruit3
+    if fruit1 == fruit2 == fruit3:
+        print 'You won!!!'
+    else:
+        print 'Maybe next time'
+    pick_game()
+
 def blackjack():
     deck = range(1,11)
-    
-    myCardsSum = 0      
+
+    myCardsSum = 0
     continuePlaying = 'y'
-    
+
     while continuePlaying == 'y' and myCardsSum < 21:
         randomCard = choice(deck)
-        deck.remove(randomCard) 
+        deck.remove(randomCard)
         myCardsSum += randomCard
         print 'You drew a ' + str(randomCard) + ', your sum is ' + str(myCardsSum) + '\n'
         if myCardsSum == 21:
@@ -54,48 +71,49 @@ def blackjack():
             return
         else:
             continuePlaying = raw_input('Do you want to draw another card? y/n \n')
-         
+
     dealerSum = randint(1, 56)
     print 'Dealer drew a ' + str(dealerSum) + '\n'
     if dealerSum < myCardsSum:
         print 'You win'
     else:
         print 'Dealer wins'
-        
-def guessing_game(lucky_number, number):
-	
-	print "You guessed %d." % number
-	
-	if number == lucky_number:
-		print "You win 1,000,000 dollars!"
-	elif number > 7: 
-		print "You lose. Too high."
-	elif number < 7:
-		print "You lose. Too low."
-	else:
-		print "You lose. You didn't follow the directions."
+    pick_game()
 
+def guessing_game(lucky_number, number):
+
+    print "You guessed %d." % number
+
+    if number == lucky_number:
+        print "You win 1,000,000 dollars!"
+    elif number > 7:
+        print "You lose. Too high."
+    elif number < 7:
+        print "You lose. Too low."
+    else:
+        print "You lose. You didn't follow the directions."
+  pick_game()
 
 def start_guessing_game():
-	print "Welcome to the guessing game."
-	number = input("Guess a number between 1 and 10.")
-	guessing_game(7, number)
-	
+    print "Welcome to the guessing game."
+    number = input("Guess a number between 1 and 10.")
+    guessing_game(7, number)
+
 def FantasyCasino():
     print """"
     From outside, the Fantasy Casino looks like a tropical paradise. Lush greenery
     sprouts from the concrete and stretches, touching the clouds. You push aside
-    the brambles and enter the large wooden doors. There are three types of slot 
+    the brambles and enter the large wooden doors. There are three types of slot
     machines - a Fairy Themed Machine, a Ghost Themed Machine and an Aquatic
     Themed Machine - which one do you pick?
 
                               .-------.
                               |Jackpot|
                   ____________|_______|____________
-                 |  __    __    ___  _____   __    |  
-                 | / _\  / /   /___\/__   \ / _\   | 
-                 | \ \  / /   //  //  / /\ \\ \  []|  
-                 | _\ \/ /___/ \_//  / /  \/_\ \ []| 
+                 |  __    __    ___  _____   __    |
+                 | / _\  / /   /___\/__   \ / _\   |
+                 | \ \  / /   //  //  / /\ \\ \  []|
+                 | _\ \/ /___/ \_//  / /  \/_\ \ []|
                  | \__/\____/\___/   \/     \__/ []|
                  |===_______===_______===_______===|
                  ||*|\_     |*| _____ |*|\_     |*||
@@ -112,8 +130,8 @@ def FantasyCasino():
                  |===_______===_______===_______===|_/
                  ||*|  ___  |*|   |   |*| _____ |*||
                  ||*| |_  | |*|  / \  |*||     ||*||
-                 ||*|  / /  |*| /_ _\ |*||*BAR*||*||              
-                 ||*| /_/   |*|   O   |*||_____||*||        
+                 ||*|  / /  |*| /_ _\ |*||*BAR*||*||
+                 ||*| /_/   |*|   O   |*||_____||*||
                  ||*|_______|*|_______|*|_______|*||
                  |===___________________________===|
                  |  /___________________________\  |
@@ -133,8 +151,8 @@ def FantasyCasino():
             ___
    __     / ::.:\\                     You pull the lever, aha! Three fairies!
  /::::'-.|::::&&&&&                   Suddenly, you feel a tap tap on your
- |::::::.\\\\ .&&&&/ \\                 shoulder. Your eyes follow. A slender 
-  \ ::..::\\\\:&&? <,(                stick? Looking up, that face... it's a 
+ |::::::.\\\\ .&&&&/ \\                 shoulder. Your eyes follow. A slender
+  \ ::..::\\\\:&&? <,(                stick? Looking up, that face... it's a
    \ ::::. \\\\ &&  _/                wand... held by a fairy! She smiles and
   /::'.:::. \\\\&&.|.                 disappears in a poof. There is a pile of
   | ..:'-.- / )___\ \\                 bills where she once stood.
